3f7be60638336724caa1eaa23a9da1abefd35c96
Badii Boussaidi <badii.boussaidi@esprit.tn>
2025-02-19 22:30:17 +0100

    fixed some code

--- backEnd/gateway/src/main/java/esprit/gateway/GatewayApplication.java
+++ backEnd/gateway/src/main/java/esprit/gateway/GatewayApplication.java
@@ -14,13 +14,19 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("Internship",
-                        r->r.path("/api/**")
-                        .uri("http://localhost:8082")).
-                build();
-     }
-
+                        r -> r.path("/api/**")
+                                .uri("http://localhost:8082"))
+                .route("Event",
+                        r -> r.path("/api/**")
+                                .uri("http://localhost:8085"))
+                .route("Supervisor",
+                        r -> r.path("/api/**")
+                                .uri("http://localhost:8081"))
+                .build();
+    }
 }
